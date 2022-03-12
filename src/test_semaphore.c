eeb93a216348a2e698abb13848cd33734e3d7a60
vwheatle <vwheatle@kent.edu>
2022-03-12 01:00:07 -0500

    Add some good comments to Semaphore example

--- src/test_semaphore.c
+++ src/test_semaphore.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <memory.h>
+#include <stdio.h>  // -> printf
+#include <stdlib.h> // -> exit, etc.
+#include <unistd.h> // -> sleep
 
-#include <pthread.h>
-#include <semaphore.h>
+#include <pthread.h>   // -> pthreads
+#include <semaphore.h> // -> semaphores
 
-#define NUM_THREADS 16
+#define NUM_THREADS 4
 sem_t thingSem;
 
 void* do_thing(void* args) {
+	// Get thread identifier
 	pthread_t me = pthread_self();
-	unsigned long int me2 = me >> 11;
-	int color = (me2 & 127) + 16;
+	
+	// I don't know, this is just for the colors to be different enough.
+	unsigned long int me2 = me >> 11; // strip out trailing zeroes
+	int color = (me2 & 127) + 16; // keep only the low 7 bits and add 16 to make a valid color value
 	
 	printf("\x1B[38;5;%dm [%lx] waiting for a turn on the video game\x1B[0m\n", color, me);
 	
+	// Try to take 1 from the semaphore.
 	sem_wait(&thingSem);
 	
 	printf("\x1B[38;5;%dm [%lx] gaming\x1B[0m\n", color, me);
-	usleep(1000);
+	usleep(250);
 	
+	// Put 1 back into the semaphore.
 	sem_post(&thingSem);
 	
 	printf("\x1B[38;5;%dm [%lx] done gaming!\x1B[0m\n", color, me);
@@ -28,23 +32,26 @@ void* do_thing(void* args) {
 
 int main() {
 	// Creates the semaphore with an initial value of 1.
+	// Change that third argument to change the amount of gamers allowed at a time.
 	sem_init(&thingSem, 0, 1);
-	
-	// Profess my love
-	printf("i <3 semaphore\n");
 	
 	pthread_t threads[NUM_THREADS];
 	
 	for (int i = 0; i < NUM_THREADS; i++) {
+		// Make some threads.
 		pthread_create(&threads[i], NULL, do_thing, NULL);
 		usleep(100);
 	}
 	for (int i = 0; i < NUM_THREADS; i++) {
+		// Wait for all of them to complete their task.
 		pthread_join(threads[i], NULL);
 	}
 	
-	// Goodbye...
+	// Destroy the semaphore mercilessly.
 	sem_destroy(&thingSem);
 	
 	exit(0);
 }
+
+// Built with:
+// gcc test_semaphore.c -pthread -o test_semaphore && ./test_semaphore
